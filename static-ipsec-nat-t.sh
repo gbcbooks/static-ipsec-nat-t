752f40f4fed8d0f0b0feccabface3b0014412836
gbcbooks <gbcbooksmj@gmail.com>
2023-05-03 05:21:55 +0800

    修改nat_local_public_ip_online_get的判断逻辑

--- static-ipsec-nat-t.sh
+++ static-ipsec-nat-t.sh
@@ -141,11 +141,11 @@ EOF
 update_nat_argument(){
     # 为空，在线获取，非空，不获取
     save_log "INFO" "updating nat_local_public_ip"
-    [ -z ${nat_local_public_ip_online_get} ] \
+    [ ! -z ${nat_local_public_ip_online_get} ] \
     && (save_log "INFO" "nat_local_public_ip_online_get FOUND !!";\
     nat_local_public_ip=$(curl -s http://myip.ipip.net | grep  -oE "([0-9]{1,3}\.){1,3}[0-9]{1,3}")\
     )
-    
+
     conntrack_result=$(ssh ${remote_ssh_user}@${remote_public_ip} -p ${remote_ssh_port} /bin/bash << EOF
     sudo /usr/sbin/conntrack -L -p udp | grep "src=${nat_local_public_ip}" | grep "dport=${remote_port}"
 EOF
