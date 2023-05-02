c7084c0e1cb3466b16a0bb67add9cf5b7ef5d537
gbcbooks <gbcbooksmj@gmail.com>
2023-05-03 05:25:02 +0800

    添加nat_local_public_ip的日志输出

--- static-ipsec-nat-t.sh
+++ static-ipsec-nat-t.sh
@@ -144,6 +144,7 @@ update_nat_argument(){
     [ ! -z ${nat_local_public_ip_online_get} ] \
     && (save_log "INFO" "nat_local_public_ip_online_get FOUND !!";\
     nat_local_public_ip=$(curl -s http://myip.ipip.net | grep  -oE "([0-9]{1,3}\.){1,3}[0-9]{1,3}")\
+    save_log "INFO" "get online nat_local_public_ip=${nat_local_public_ip}"\
     )
 
     conntrack_result=$(ssh ${remote_ssh_user}@${remote_public_ip} -p ${remote_ssh_port} /bin/bash << EOF
